e8d97ed090c15bcd4bc45755e54177ec906de8d5
Jose M. Serra Jr <joseserra803@gmail.com>
2023-03-20 13:53:56 -0400

    Fixed Header.php Added.

--- views/header.php
+++ views/header.php
@@ -34,7 +34,7 @@ echo'
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Display Stats</a>
