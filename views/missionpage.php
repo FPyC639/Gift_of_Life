34fe2f0d31cfcfb89ed927309389beb3e17ced8c
Jose M Serra Jr <joseserra803@gmail.com>
2023-04-11 21:17:00 -0400

    Added content to Mission Page.

--- views/missionpage.php
+++ views/missionpage.php
@@ -1,9 +1,7 @@
 <?php
 include "header.php";
 
-
 echo"   
-
 <div class='card bg-white text-white'>
   <img class='card-img bg-transparent' src='images/tangerine.png' alt='Card image' >
   <div class='card-img-overlay'>
@@ -20,17 +18,38 @@ echo"
 
   <p style='color:white;'>Live Testimonals:<p>
 
-  <div class='card bg-transparent border-0'>
-      <center> <h1 style='color:white;'>Anonymous</h1> </center>
-      <p style='color:white;'>
-      I don't know who you are, but I want to express my deepest gratitude to you for giving me the gift of life. Thanks to your selfless act of donating your organ, I have a second chance at life and the opportunity to spend more time with my loved ones.
-
-      I can only imagine the courage and compassion it takes to make such a decision, and I want you to know that your generosity has not gone unnoticed. You have made an incredible difference in my life and in the lives of my family and friends.
-
-      Although we may never meet, I will always carry you in my heart and will never forget the incredible gift you have given me. You are a true hero and an inspiration to us all.
-      </p>
-      <center><img class='d-block w-25' src='images/silo.svg' alt='Anonymous'></center>
+  <div class='thumbnail'>
+  <a href='#' onclick=\"toggleContent( './testimonals/test1.html' )\">
+  <img src='thumbnail.jpg' alt='Thumbnail1'>
+  </a>
   </div>
+  <div class='thumbnail'>
+  <a href='#' onclick=\"toggleContent( './testimonals/test2.html' )\">
+  <img src='thumbnail.jpg' alt='Thumbnail2'>
+  </a>
+  </div>
+<div id='content'></div>
+
+<script>
+  var loaded = false;
+
+  function toggleContent(url) {
+    if (loaded) {
+      document.getElementById('content').innerHTML = '';
+      loaded = false;
+    } else {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById('content').innerHTML = this.responseText;
+          loaded = true;
+        }
+      };
+      xhttp.open('GET', url, true);
+      xhttp.send();
+    }
+  }
+</script>
 
 
   </div>
