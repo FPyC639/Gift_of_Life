a0f83297a37bc1a20c72072cf1fdd9615ede4690
MikeySz <60848730+MikeySz@users.noreply.github.com>
2023-04-25 13:33:32 -0400

    The website redesign

---
+++ GiftOfLife- Michael Edits/header.php
@@ -0,0 +1,76 @@
+ <?php
+echo "<!doctype html> <html lang='en'>";
+echo"<head>
+    <meta charset='utf-8' />
+    <meta name='description' content='Gift of Life Website' />
+    <meta author='Gift of Life Team' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <title>Gift of Life</title>
+<!--------------------------------------------- Dependencies ------------------------------------------------------------------------------------------------>
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD' crossorigin='anonymous' />
+
+  <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' integrity='sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN' crossorigin='anonymous'></script>
+
+  <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
+
+<script src='https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
+
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js'></script>
+<!----------------------------------------------------------------->
+  </head>";
+
+echo'  
+<body>
+<!-------------Header--------------->
+<div class ="header" style ="background-color: #fca247;">
+
+<div class= "d-flex flex-row">
+<nav class="navbar navbar-expand-lg navbar-light" style= "background-color: #fca247">
+  <a class="navbar-brand" href="#">Gift of Life</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="stats.php">Display Stats</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="missionpage.php">Mission Page</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="founders.php">Founders</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Login/Signup
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: #ffeace;">
+          <a class="dropdown-item" href="HCI.php">Sign up</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="HCI.php">Login</a>
+        </div>
+      </li>
+    </ul>
+
+
+  </div>
+</nav>
+
+</div>
+
+
+
+
+   
+
+</div>
+ ';
+
+  ?>
